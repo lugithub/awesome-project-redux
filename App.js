4c9b4943f7083cb1bf84dc4d3068aa7fb30379a3
lugithub <lu.bin.lu.bin@gmail.com>
2017-10-17 00:02:48 -0400

    try redux first time. it still does not work

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
 
-export default class App extends React.Component {
+//action creator
+function toggleShowText() {
+  return { type: 'Toggle' };
+}
+
+function showText(state = true, action) {
+  switch (action.type) {
+    case 'Toggle':
+      return !state;
+    default:
+      return state;
+  }
+}
+
+//reducer
+const showTextApp = combineReducers({
+  showText
+})
+
+let store = createStore(showTextApp);
+
+const Blink = ({ text, showText, onTodoClick }) => {
+  return (<Text onClick={onTodoClick}>
+    {showText ? text : ' '}
+    </Text>);
+};
+
+const mapStateToProps = state => {
+  return {
+    showText: state.showText
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoClick: () => {
+      dispatch(toggleShowText())
+    }
+  }
+}
+
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Blink text='I love to blink' showText={true} onTodoClick={() => {}} />
+        <Blink text='I love to blink' showText={true} onTodoClick={() => {}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
